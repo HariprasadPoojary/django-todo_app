e845935c0cbd47cdc4afeff5fbb7cb91fe5540cf
Hariprasad <haridx.poojary@gmail.com>
2020-11-15 18:19:24 +0530

    Connected app to postgres db

--- Todo_App/settings.py
+++ Todo_App/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "Todo_App.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo_db",
+        "USER": "postgres",
+        "PASSWORD": "haridx12",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- todo/models.py
+++ todo/models.py
@@ -10,7 +10,7 @@ class Todo(models.Model):
     )
 
     title = models.CharField(max_length=120, null=False)
-    state = models.CharField(choices=STATE_MODEL)
+    state = models.CharField(max_length=15, choices=STATE_MODEL)
 
     def __str__(self) -> str:
         return self.title
